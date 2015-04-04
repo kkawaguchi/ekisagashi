a5793c5d49352d58f8a13b74df9e5add9add7b71
kawaguchi <qqvs4bc9k@grace.ocn.ne.jp>
2015-04-04 16:14:33 +0900

    サーバ用プロジェクトの追加

---
+++ Ekisagashi/EkisagashiServer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EkisagashiServer.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Ekisagashi/EkisagashiServer/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace EkisagashiServer.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
