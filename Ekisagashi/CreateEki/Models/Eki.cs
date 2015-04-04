0bafe68abc4dc76bb2db307d3df74aeb061806ac
kawaguchi <qqvs4bc9k@grace.ocn.ne.jp>
2015-04-04 20:34:58 +0900

    データ読み込みツール作成

---
+++ Ekisagashi/CreateEki/Models/Eki.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EkisagashiServer.Models
+{
+    public class Eki
+    {
+        public string EkiId { get; set; }
+        public string Name { get; set; }
+        public string RosenId { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
